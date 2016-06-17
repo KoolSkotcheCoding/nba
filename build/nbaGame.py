51c3145d016cb6a815d142bdf6704fd4a4a137ad
Keith Schumacher <keithschumacher.daily@gmail.com>
2016-06-16 19:27:12 -0500

    clean up and comment nbaGame and nbaMoment

--- build/nbaGame.py
+++ build/nbaGame.py
@@ -1,11 +1,21 @@
-#moments
-#contains (quarter, ?, game clock (seconds), shot clock (seconds),
-#          ?, player+ball position===position)
+#The NBA_game class is meant to be a tool for working with the movement JSON files provided by the NBA.
+#The movement JSON files conain a series of 'events'. Each 'event' contains a series of 'moments'.
+#Each moment is a snapshot of an NBA game at a given time (shot clock and game clock), containing locations of the ball and all players. 
+#An 'event' represents a sequence of moments. I am still unsure of the exact meaning of a an 'event'. 
+#However, an event is not always:
+    #a (basketball) possession. There are events consisting of a shot by team A, rebound by team B, made shot by team B (2 possessions). 
+    #'unique'. Some events seem to overlap in the sense that a chronological ordering of all moments in all events would 'shuffle' the events.
+        #(in other words, the union between game times contained any event with any other event is not necessarily null).
+        #Due to data quality issues (to be further explored), a chronological ordering of all moments within a game is not advisiable.
+        #Thus (for the time being) the event-moment structure provided by the NBA is essentially respected/maintaned by this NBA_game class.
+#Initialize: To instantiate, call class with output of json.load(movementFileFromNBA.json)
 
-#shot clock can be empty (none, nonetype, etc.)
-#position contains: (team ID, player ID, x position, y position, z position)
 
-#For ball, team ID = -1, player ID = -1, z position could be physically meaningful
+#Expected JSON structure
+#A 'moments' contains (quarter, ?, game clock (seconds), shot clock (seconds),?, player+ball position)
+#Shot clock can be empty (none, nonetype, etc.).
+#player+ball position contains: (team ID, player ID, x coordinate, y coordinate, z coordinate).
+#For ball, team ID = -1, player ID = -1, z coordinate is physically meaningful.
 #For players, z position is always zero. 
 import pandas as pd
 import nbaMoment
@@ -13,31 +23,50 @@ import nbaMoment
 class NBA_game:
     def __init__(self, rawGame):
 	self.rawGame=rawGame
-        self.home=rawGame["events"][0]["home"]
-        self.away=rawGame["events"][0]["visitor"]
-        self.gameDate=rawGame["gamedate"]
-        self.gameID=rawGame["gameid"]
-        
+#        self.home=rawGame["events"][0]["home"]
+#        self.away=rawGame["events"][0]["visitor"]
+#        self.gameDate=rawGame["gamedate"]
+#        self.gameID=rawGame["gameid"]
+#        self.gameIndex=['gameClock','shotClock','quarter','eventID','-1'+'_x','-1'+'_y','-1'+'_z']
+#        for playerID in self.players['playerid']:
+#            self.gameIndex.append(str(playerID)+'_x')
+#            self.gameIndex.append(str(playerID)+'_y')
+#            self.gameIndex.append(str(playerID)+'_z')
+#
+
+
+#        self.momentCnt=0
+#        for event in self.rawGame["events"]:
+#            for moment in event["moments"]:
+#                if moment:
+#                    self.momentCnt+=1
+
+        ###########################################################################3
+
+
+
+
+
         self.homePlayers=pd.DataFrame(rawGame["events"][0]["home"]["players"])
         self.awayPlayers=pd.DataFrame(rawGame["events"][0]["visitor"]["players"])
 
         self.players=self.homePlayers.append(self.awayPlayers,ignore_index=True)
         
-        self.gameIndex=['gameClock','shotClock','quarter','eventID','-1'+'_x','-1'+'_y','-1'+'_z']
-        for playerID in self.players['playerid']:
-            self.gameIndex.append(str(playerID)+'_x')
-            self.gameIndex.append(str(playerID)+'_y')
-            self.gameIndex.append(str(playerID)+'_z')
         
-	
+    ##getEventData##
+    #Returns: a tuple containing True and a Pandas Dataframe if the specified event ID is found in the game.
+    #Or False and an empty list if the event ID is not present in the game (or if there are no moments within the specified event ID.
+    #Parameters:
+        #eventID - Event ID of desired event.
+        #ball - optional argument (False by default). True - return ball movement data
+        #time - optional argument (False by default). True - return shot clock and gameclock data
+        #players - optional argument (False by default). List of player IDs to return. 
+            #If a player ID in players is not active in the event (not on the court), that player ID will not be present in the returned dataframe.
+            #If a player ID is only active (on the court) for some of the moments within the event, the dataframe will contain a column for the player
+            #with Nan values for the moments the player ID is not active. 
 	        
         
         
-        self.momentCnt=0
-        for event in self.rawGame["events"]:
-            for moment in event["moments"]:
-                if moment:
-                    self.momentCnt+=1
         
     def getEventData(self,eventID,ball=False,time=False,players=False):
         for event in self.rawGame["events"]:
@@ -48,41 +77,13 @@ class NBA_game:
                 for moment in event["moments"]:
                     if moment:
                         if tmp==0:
-                            target_event=nbaMoment.NBA_moment(moment,event['eventId'],ball,time,playerList=players).momentDF
+                            target_event=nbaMoment.NBA_moment(moment,event['eventId'],ball,time,players).momentDF
                             tmp+=1
                         else:
-                            target_event=target_event.append(nbaMoment.NBA_moment(moment,event['eventId'],ball,time,playerList=players).momentDF,ignore_index=True)
+                            target_event=target_event.append(nbaMoment.NBA_moment(moment,event['eventId'],ball,time,players).momentDF,ignore_index=True)
                             tmp+=1
                 if len(target_event)==0:
                     return False,[]
                 else:
                     return True,target_event
         return False,[]
-
-
-
-
-
-    def getEvent(self,eventID):
-	for event in self.rawGame["events"]:
-            #print event["eventId"]
-            emptyMom=False
-	    if event["eventId"]==str(eventID):
-		tmp=0
-                target_event=[]
-		for moment in event["moments"]:
-		    if moment:
-		        if tmp==0:
-			    target_event=nbaMoment.NBA_moment(moment,event['eventId'],playerList=self.players['playerid']).momentDF
-			    tmp+=1
-                        else:
-                            target_event=target_event.append(nbaMoment.NBA_moment(moment,event['eventId'],playerList=self.players).momentDF,ignore_index=True)
-			    tmp+=1
-                if len(target_event)==0:
-                    return False,[]
-                else:
-                    return True,target_event
-        return False,[]
-
-def timeElapsed(event):
-    return event[:][['gameClock','shotClock']]
--- build/nbaMoment.py
+++ build/nbaMoment.py
@@ -1,32 +1,32 @@
+#The NBA_moment class is meant to be a tool for working with single 'moments' within the movement JSON files provided by the NBA.
+
+#Expected JSON structure
+#A 'moments' contains (quarter, ?, game clock (seconds), shot clock (seconds),?, player+ball position)
+#Shot clock can be empty (none, nonetype, etc.).
+#player+ball position contains: (team ID, player ID, x coordinate, y coordinate, z coordinate).
+#For ball, team ID = -1, player ID = -1, z coordinate is physically meaningful.
+#For players, z position is always zero. 
+
+
 import math
 import pandas as pd
 class NBA_moment:
     def __init__(self,rawMoment,eventID,ball=False,time=False,playerList=None):#,home,away):
         self.quarter=rawMoment[0]
+#collect player and ball positions into dictionary with indices (playerID,'x') where x indicates the x position.
         if rawMoment:
             self.momentDict={}
-            self.momentDict['gameClock']=rawMoment[2]
-            self.momentDict['shotClock']=rawMoment[3]
-        
-        
-        
-            #collect player and ball positions into dictionary with indices (playerID,'x') where x indicates the x position.
-            if ball is False and time is False and playerList is False:
+            if time is not False:
+                self.momentDict['gameClock']=rawMoment[2]
+                self.momentDict['shotClock']=rawMoment[3]
+            if ball is not False or playerList is not False:
                 for player in rawMoment[5]:
-                    if player[1]==-1:
+                    if player[1]==-1 and ball is True:
                         self.momentDict[(player[1],'x')]=round(player[2],2)
                         self.momentDict[(player[1],'y')]=round(player[3],2)
                         self.momentDict[(player[1],'z')]=round(player[4],2)
-            else:
-                if playerList is not None:
-                    for player in rawMoment[5]:
+                    elif playerList is not None and playerList is not False:
                         if player[1] in playerList.values:
                             self.momentDict[(player[1],'x')]=round(player[2],2)
                             self.momentDict[(player[1],'y')]=round(player[3],2)
-                        elif player[1]==-1:
-                            self.momentDict[(player[1],'x')]=round(player[2],2)
-                            self.momentDict[(player[1],'y')]=round(player[3],2)
-                            self.momentDict[(player[1],'z')]=round(player[4],2)
-                
-                
             self.momentDF=pd.DataFrame(self.momentDict, index=[0])
